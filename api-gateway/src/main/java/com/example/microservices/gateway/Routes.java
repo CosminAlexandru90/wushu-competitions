eb9ddb204f9fbc4a157f65138db5c6ac2d4a77d8
Cosmin Alexandru <cosmin.alexandru@stsnet.ro>
2025-03-25 15:23:35 +0200

    inventory service

---
+++ api-gateway/src/main/java/com/example/microservices/gateway/Routes.java
@@ -0,0 +1,4 @@
+package com.example.microservices.gateway;
+
+public class Routes {
+}
--- order-service/src/test/java/com/example/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.microservices.order;
 
+import com.example.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
@@ -14,44 +16,46 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-	@ServiceConnection
-	static MySQLContainer mySQLContainer= new MySQLContainer("mysql:8.3.0");
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 
-	@LocalServerPort
-	private Integer port;
+    @LocalServerPort
+    private Integer port;
 
-	@BeforeEach
-	void setUp() {
-		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = port;
-	}
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
 
-	static {
-		mySQLContainer.start();
-	}
+    static {
+        mySQLContainer.start();
+    }
 
-	@Test
-	void shouldCreateOrder() {
-		String submitOrderJson = """
-				{
-				     "skuCode": "iPhone_15",
-				     "price":1000,
-				     "quantity": 1
-				 }
-			""";
-		var responseBodyString = RestAssured.given()
-				.contentType("application/json")
-				.body(submitOrderJson)
-				.when()
-				.post("/api/order")
-				.then()
-				.log().all()
-				.statusCode(201)
-				.extract()
-				.body().asString();
-		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
-	}
+    @Test
+    void shouldSubmitOrder() {
+        String submitOrderJson = """
+                	{
+                	     "skuCode": "iPhone_15",
+                	     "price":1000,
+                	     "quantity": 1
+                	 }
+                """;
+        InventoryClientStub.stubInventoryCall("iPhone_15", 1);
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+        assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+    }
 
 }
